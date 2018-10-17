963b2fd4f4b481413601fae409f700557b3588c7
VexeDev <44173192+VexeDev@users.noreply.github.com>
2018-10-16 20:42:05 -0400

    Item SO
    Set up an item SO system for consumables that would be in the item shop and maybe inventory. You simply put the prefab into the shop window, and then you can set it to whatever scriptable object (that is a consumable) you like and it will display the information. Actual money, purchasing system, ect is not done yet.

---
+++ Nameless/Assets/Code/Shop/Consumable/DisplayCosumables.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayCosumables : MonoBehaviour {
+
+    public ConsumableSO item;
+
+    public Image sprite;
+    public Text nameValue;
+    public Text attackValue;
+    public Text healthValue;
+    public Text speedValue;
+    public Text costValue;
+
+	// Use this for initialization
+	void Start () {
+        sprite.sprite = item.itemImage;
+        nameValue.text = item.name;
+        attackValue.text = item.attackBoost.ToString();
+        speedValue.text = item.speedBoost.ToString();
+        healthValue.text = item.healthBoost.ToString();
+        costValue.text = item.cost.ToString();
+	}
+	
+}
