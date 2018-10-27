6364cbd6a1acb74d32f339043fdd8bad161be278
Mitchch <Mitchellhoward74@gmail.com>
2018-10-27 16:22:40 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/ProjectNameless/Nameless""
    This reverts commit 548a38e0df01718da45197cdf98a6061318b4c86.

--- Nameless/Assets/Code/DialogueWithAudioSO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Dialogue with audio", menuName = "Dialogue/with audio", order = 1)]
-public class DialogueWithAudioSO : DialogueSO {
-
-    public AudioClip voice;
-}
