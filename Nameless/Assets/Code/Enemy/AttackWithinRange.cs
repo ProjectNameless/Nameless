ba99a28a3c568d20f227780ff20e25c1bc13cc63
JustLoren <loren.naegle@gmail.com>
2018-10-29 10:12:29 -0500

    Adding Loren's Scene. Added enemy prefab that attacks with sword. Player does not yet take damage.

---
+++ Nameless/Assets/Code/Enemy/AttackWithinRange.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackWithinRange : MonoBehaviour {
+    public GameObject target;
+    public float attackRange;
+    public Animation anim;
+    public MeleeWeaponTrail attackTrail;
+
+    public bool autoTargetPlayer = true;
+    private List<GameObject> damageList = new List<GameObject>();
+
+    //Is called when the attack succeeds
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!damageList.Contains(other.gameObject))
+        {
+            var weapon = GetComponentInChildren<Weapon>();
+            Debug.Log("Apply " + weapon.damage + " damage to target.");
+            damageList.Add(other.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (autoTargetPlayer)
+            target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        if (anim.isPlaying)
+            return;
+        else if (attackTrail != null)
+            attackTrail.Emit = false;
+
+        var diff = target.transform.position - transform.position;
+        if (diff.magnitude <= attackRange)
+        {
+            damageList.Clear();
+            anim.Play();
+            if (attackTrail != null)
+                attackTrail.Emit = true;
+        }
+    }
+}
---
+++ Nameless/Assets/Code/Movement/SteerTowards.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteerTowards : MonoBehaviour {
+    public GameObject steeringTarget;
+    public float steeringDistance;
+    public float steeringForce;
+    private Rigidbody rb;
+
+    public bool autoTargetPlayer = true;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (autoTargetPlayer)
+            steeringTarget = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(new Vector3(steeringTarget.transform.position.x, transform.position.y, steeringTarget.transform.position.z));
+
+        var diff = steeringTarget.transform.position - transform.position;
+
+        if (diff.magnitude > steeringDistance)
+        {
+            rb.AddForce(diff.normalized * steeringForce);
+        } else
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
