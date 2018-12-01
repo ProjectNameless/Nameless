65a6ed15904437abb7878a8e1e99aaf1e4ccc1bb
sholom1 <42785937+sholom1@users.noreply.github.com>
2018-12-01 00:21:08 -0500

    Main menu added
    Lots of files added mainly due to builds

---
+++ Nameless/Assets/Code/SceneSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneSwitcher : MonoBehaviour {
+
+    public GameObject loadingBarAsset;
+	public void LoadScene(int num)
+    {
+        StartCoroutine(LoadingBar(SceneManager.LoadSceneAsync(num)));
+    }
+    IEnumerator LoadingBar(AsyncOperation operation)
+    {
+        GameObject loadingBar = (GameObject)Instantiate(loadingBarAsset);
+        Slider slider = loadingBar.GetComponentInChildren<Slider>();
+        Text percentage = loadingBar.GetComponentInChildren<Text>();
+        while (!operation.isDone)
+        {
+            slider.value = operation.progress;
+            percentage.text = "Loading: " + Mathf.RoundToInt(operation.progress * 100) + "%";
+            yield return null;
+        }
+    }
+}
